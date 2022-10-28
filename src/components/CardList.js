4fee9bdb33ce68b5fc9aab091eca1fea167f1478
Ruby Sapkota <rubysapkota0@gmail.com>
2022-10-27 22:15:52 -0400

    First commit

---
+++ src/components/CardList.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = ({robots}) => {
+
+    
+    return(
+        <React.StrictMode>
+    <div>
+      {robots.map((user,i) => {
+        return ( 
+            <Card 
+            key={i} 
+            id = {robots[i].id} 
+            name = {robots[i].name}
+            email= {robots[i].email}
+            /> 
+            );
+    })
+       }
+    </div>
+    
+    
+  </React.StrictMode>
+
+
+
+    );
+
+
+}
+
+export default CardList
